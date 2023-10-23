1857d192fc0d58df15f8694bcd4e298cb10c17bd
Borja <elbarmandev@gmail.com>
2023-10-23 15:38:16 +0200

    modules in javascript

--- src/js/functions/favorite.js
+++ src/js/functions/favorite.js
@@ -1,14 +1,12 @@
 export function initializeFavorites() {
-    
     const heartIcons = document.querySelectorAll('.red-heart');
 
     heartIcons.forEach(heartIcon => {
-
         const productId = heartIcon.closest('.product-card').querySelector('h2').textContent;
-
         const isFavorite = localStorage.getItem(`favoriteProduct_${productId}`) === 'true';
+
         if (isFavorite) {
-            heartIcon.src = '../../../public/img/icons/HeartLoveLikeRed.svg';
+            heartIcon.src = '../../../public/img/icons/HeartLoveLikeRed.svg'; 
         }
 
         heartIcon.addEventListener('click', function () {
@@ -18,14 +16,13 @@ export function initializeFavorites() {
 }
 
 export function toggleFavorite(heartIcon, productId) {
-
     const localStorageKey = `favoriteProduct_${productId}`;
 
-    if (heartIcon.src.endsWith('HeartLoveLike.svg')) {
-        heartIcon.src = '../../../public/img/icons/HeartLoveLikeRed.svg';
+    if (heartIcon.src.endsWith('../../../public/icons/HeartLoveLike.svg')) { 
+        heartIcon.src = '../../../public/icons/HeartLoveLikeRed.svg'; 
         localStorage.setItem(localStorageKey, 'true');
     } else {
-        heartIcon.src = '../../../public/img/icons/HeartLoveLike.svg';
-        localStorage.setItem(localStorageKey, 'false'); 
+        heartIcon.src = '../../../public/img/icons/HeartLoveLike.svg'; 
+        localStorage.setItem(localStorageKey, 'false');
     }
 }
