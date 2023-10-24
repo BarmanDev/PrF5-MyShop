dbfc786648b68cd77698d6ee8ae1dd7ab5490172
Alicia González Castro <aliglezcastro@gmail.com>
2023-10-24 15:33:19 +0200

    add new img

--- src/js/app.js
+++ src/js/app.js
@@ -9,7 +9,7 @@ punto.forEach((cadaPunto, i) => {
     punto[i].addEventListener('click', ()=>{
        
         let posicion = i
-        let operacion = posicion * -25
+        let operacion = posicion * -20
 
         grande.style.transform = `translateX(${operacion}%)`
 
